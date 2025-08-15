a22112e1373c2bd94ff37b24911d1964827fc8ad
ONandoTI <contatonandoti@gmail.com>
2025-08-15 17:50:29 -0300

    Create style.css

---
+++ style.css
@@ -0,0 +1,14 @@
+<!-- index.html -->
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Calculadora</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Calculadora Simples</h1>
+  <!-- Interface simulada -->
+  <script src="script.js"></script>
+</body>
+</html>
